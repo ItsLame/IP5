b443a35fe2d19f55d0fe429033b5c76b3e836c63
Elicia Au Duong <eliciaauduong@outlook.com>
2022-07-23 23:09:47 +1000

    Fixed error messages

--- .workspaces/ip5/contracts/Transfer.sol
+++ .workspaces/ip5/contracts/Transfer.sol
@@ -8,34 +8,44 @@ import './BookFactory.sol';
 
 contract Transfer
 {
+    BookFactory bookFactory;
+
     function buy(address _bookAddress) payable public {
         Book book = Book(_bookAddress);
+        uint price = book.bookPrice();
+        string memory title = book.bookTitle();
+        string memory author = book.bookAuthor();
+        bool forSale = book.forSale();
         // check if the book is for sale
-        require(book.forSale = true, "Book is not for sale");
+        require(forSale == true, "Book is not for sale");
         
         // transfer buyer -> owner (money)
         // money send into owner account
-        address sendTo = book.owner;
-        bool sent = sendTo.send(book.bookPrice); // gas limit 2300 gas
+        address payable sendTo = book.ownerPayable();
+        bool sent = sendTo.send(price); // gas limit 2300 gas
         require(sent, "Transfer failed. Ether not sent");
 
         // create a new copy of the book for buyer
-        newBook(book.bookTitle, book.bookAuthor, book.bookPrice); // how to call this function properly??
+        bookFactory.newBook(title, author, price);
     }
 
-    function sell(address _bookAddress) public {
+    function sell(address _bookAddress) public view {
         Book book = Book(_bookAddress);
+        bool forSale = book.forSale();
+        address owner = book.ownerAddress();
         // owner only action
-        require(msg.sender = book.owner, "Only owner can mark book as for sale");
+        require(msg.sender == owner, "Only owner can mark book as for sale");
         // turn on book for sale
-        book.forSale = true;
+        forSale = true;
     }
 
-    function offmarket(address _bookAddress) public {
+    function offmarket(address _bookAddress) public view {
         Book book = Book(_bookAddress);
+        bool forSale = book.forSale();
+        address owner = book.ownerAddress();
         // owner only action
-        require(msg.sender = book.owner, "Only owner can mark book as for sale");
+        require(msg.sender == owner, "Only owner can mark book as for sale");
         // turn off book for sale
-        book.forSale = false;
+        forSale = false;
     }
 }
