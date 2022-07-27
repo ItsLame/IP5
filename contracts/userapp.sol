36a4bbcac123cbc395436b794f39b292a2817730
Jesse Merhi <j.merhi@student.unsw.edu.au>
2022-07-27 16:46:43 +1000

    Frontend start

--- contracts/userapp.sol
+++ contracts/userapp.sol
@@ -18,39 +18,43 @@ contract UserApp {
     BookFactory public bookFactory;
     Transfer public transferC;
 
-    function publishbook(string memory  bookTitle, string memory  bookAuthor, uint256  bookPrice ) public {
+    function publishbook(
+        string memory bookTitle,
+        string memory bookAuthor,
+        uint256 bookPrice
+    ) public {
         bookFactory.newBook(bookTitle, bookAuthor, bookPrice, msg.sender);
     }
 
-
-    function str2int(string memory numString) public pure returns(uint) {
-        uint  val=0;
-        bytes   memory stringBytes = bytes(numString);
-        for (uint  i =  0; i<stringBytes.length; i++) {
-            uint exp = stringBytes.length - i;
+    function str2int(string memory numString) public pure returns (uint256) {
+        uint256 val = 0;
+        bytes memory stringBytes = bytes(numString);
+        for (uint256 i = 0; i < stringBytes.length; i++) {
+            uint256 exp = stringBytes.length - i;
             bytes1 ival = stringBytes[i];
             uint8 uval = uint8(ival);
-           uint jval = uval - uint(0x30);
-   
-           val +=  (uint(jval) * (10**(exp-1))); 
-             
+            uint256 jval = uval - uint256(0x30);
+
+            val += (uint256(jval) * (10**(exp - 1)));
         }
         return val;
     }
-    function buyBook(address  bookAddress) payable public {
+
+    function buyBook(address bookAddress) public payable {
         Book book = Book(bookAddress);
-        require(msg.value == book.bookPrice(),"Need to send exact amount of Wei");
+        require(
+            msg.value >= book.bookPrice(),
+            "Need to send exact amount of Wei"
+        );
         // transfer buyer -> owner (money)
         // money send into owner account
         address payable owner = book.ownerPayable();
-        owner.transfer(msg.value); 
-        transferC.buy(bookAddress,msg.sender);
+        owner.transfer(msg.value);
+        transferC.buy(bookAddress, msg.sender);
     }
 
-    constructor(address bookfactoryAd, address transferContract)
-    { 
-        bookFactory = BookFactory(bookfactoryAd); 
+    constructor(address bookfactoryAd, address transferContract) {
+        bookFactory = BookFactory(bookfactoryAd);
         transferC = Transfer(transferContract);
     }
-
 }
